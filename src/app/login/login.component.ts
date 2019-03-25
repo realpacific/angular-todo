3c1127ea337bbc21400799890264acb77cdbb7e3
Prashant <prashantbarahi@gmail.com>
2019-03-26 00:05:59 +0545

    Login created and linked to welcome page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { invalid } from '@angular/compiler/src/render3/view/util';
+import { Route, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: [ './login.component.css' ]
 })
 export class LoginComponent implements OnInit {
+	username = 'prashant';
+	password = '';
+	errorMessage = 'Invalid credentials';
+	invalidLogin = false;
 
-  constructor() { }
+	//Dependency Injection; this will be available as member
+	// variable by default if private is not specified
+	constructor(private router: Router) {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
+	handleLogin() {
+		if (this.username === 'prashant' && this.password === 'password') {
+			this.invalidLogin = false;
+
+			this.router.navigate([ 'welcome', this.username ]);
+		} else this.invalidLogin = true;
+	}
 }
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+	selector: 'app-welcome',
+	templateUrl: './welcome.component.html',
+	styleUrls: [ './welcome.component.css' ]
 })
 export class WelcomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	// get the currently activated route
+	constructor(private route: ActivatedRoute) {}
+  username = '';
+  
+	ngOnInit() {
+		this.username = this.route.snapshot.params['name'];
+	}
 }
