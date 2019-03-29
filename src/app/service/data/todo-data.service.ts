27253c18f5d19a717564536eb9459556f13eb438
Prashant <prashantbarahi@gmail.com>
2019-03-30 00:32:08 +0545

    Constants file added

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from 'src/app/list-todos/list-todos.component';
+import {API_URL} from 'src/app/app.constants';
 
 @Injectable({
 	providedIn: 'root'
@@ -9,22 +10,22 @@ export class TodoDataService {
 	constructor(private http: HttpClient) {}
 
 	fetchAllTodos(username: string) {
-		return this.http.get<Todo[]>(`http://localhost:8080/users/${username}/todos`);
+		return this.http.get<Todo[]>(`${API_URL}/users/${username}/todos`);
 	}
 
 	fetchSingleTodo(username: string, id: number) {
-		return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+		return this.http.get<Todo>(`${API_URL}/users/${username}/todos/${id}`);
 	}
 
 	deleteTodoById(username: string, id: number) {
-		return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
+		return this.http.delete(`${API_URL}/users/${username}/todos/${id}`);
 	}
 
 	updateTodo(username: string, id: number, todo: Todo) {
-		return this.http.put(`http://localhost:8080/users/${username}/todos/${id}`, todo);
+		return this.http.put(`${API_URL}/users/${username}/todos/${id}`, todo);
 	}
 
 	saveTodo(username: string, todo: Todo) {
-		return this.http.post(`http://localhost:8080/users/${username}/todos`, todo);
+		return this.http.post(`${API_URL}/users/${username}/todos`, todo);
 	}
 }
